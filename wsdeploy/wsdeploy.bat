008214dcddbd860e90d717e71db97dddf6f7a4f5
Samuel Waymouth <sam.waymouth@gmail.com>
2012-03-01 10:38:54 +0000

    Updated the build.xml to create a distribution.  wsdeploybat and wsdeploy.sh updated to remove references to extra jython.jar.

--- wsdeploy/wsdeploy.bat
+++ wsdeploy/wsdeploy.bat
@@ -8,4 +8,4 @@ SET WAS_HOST=%1
 SET WAS_PORT=%2
 SET ENV=%3
 SET ACTION=%4
-%WAS_HOME%/bin/wsadmin.bat -lang jython -host %WAS_HOST% -port %WAS_PORT% -wsadmin_classpath "%WSDEPLOY_LIB%/log4j-1.2.15.jar;%WSDEPLOY_CONF%;%WSDEPLOY_LIB%/jython.jar" -f %WSDEPLOY_SRC%/wsdeploy.py %ENV% %ACTION%
+%WAS_HOME%/bin/wsadmin.bat -lang jython -host %WAS_HOST% -port %WAS_PORT% -wsadmin_classpath "%WSDEPLOY_LIB%/log4j-1.2.15.jar;%WSDEPLOY_CONF%" -f %WSDEPLOY_SRC%/wsdeploy.py %ENV% %ACTION%
